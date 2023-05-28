ca16a3b6dd75188cd699d3b5293f8d8db81ab079
vedr02 <118558089+vedr02@users.noreply.github.com>
2023-05-28 15:38:38 +0530

    Add files via upload

---
+++ FILTER/js/main.js
@@ -0,0 +1,259 @@
+    let products =[
+
+    {
+        id: 1,
+        product_name: "Men Solid Polo Neck Green T-Shirt",
+        price: 299,
+        category: "T-Shirt",
+        pic: "https://rukminim1.flixcart.com/image/612/612/xif0q/t-shirt/s/n/x/l-db1024-15-3bros-original-imagbbn9nknyhnf7-bb.jpeg?q=70",
+    },
+    {
+        id: 2,
+        product_name: "Men Regular Fit Solid Formal Shirt",
+        price: 389,
+        category: "Formal Shirt",
+        pic: "https://rukminim1.flixcart.com/image/612/612/l0fm07k0/shirt/v/m/a/s-sh-55-getchi-original-imagc8fd4wye86t7.jpeg?q=70",
+    },
+    {
+        id: 3,
+        product_name: "Men Skinny Mid Rise Blue Jeans",
+        price: 499,
+        category: "jeans",
+        pic: "https://rukminim1.flixcart.com/image/612/612/jw6pifk0/jean/k/9/v/36-9114351-roadster-original-imafgwhrkjvyrrez.jpeg?q=70",
+    },
+    {
+        id: 4,
+        product_name: "Printed Men Blue Regular Shorts",
+        price: 263,
+        category: "Shorts",
+        pic: "https://rukminim1.flixcart.com/image/612/612/xif0q/short/s/j/o/xxl-tnvshortabs1-tripr-original-imagbwnzs2uen9qz-bb.jpeg?q=70",
+    },
+    {
+        id: 5,
+        product_name: "Solid Men Polo Neck Red T-Shirt",
+        price: 761,
+        category: "T-Shirt",
+        pic: "https://rukminim1.flixcart.com/image/660/792/kupuljk0/t-shirt/g/6/n/m-ustsh0517-u-s-polo-assn-original-imag7rx59yqucnar.jpeg?q=50",
+    },
+    {
+        id: 6,
+        product_name: "Men Slim Fit Solid Formal Shirt",
+        price: 409,
+        category: "Formal Shirt",
+        pic: "https://rukminim1.flixcart.com/image/660/792/kh80vww0-0/shirt/o/y/c/m-fo-shrt-499-wht-5th-anfold-original-imaeue3b4zycypnw.jpeg?q=50",
+    },
+    {
+        id: 7,
+        product_name: "Regular Men Grey Jeans",
+        price: 299,
+        category: "Jeans",
+        pic: "https://rukminim1.flixcart.com/image/660/792/kk2wl8w0/jean/0/f/h/36-r1101-rasso-original-imafzgbhbgufmfmu.jpeg?q=50",
+    },
+    {
+        id: 8,
+        product_name: "Solid Men Black Sports Shorts",
+        price: 199,
+        category: "Shorts",
+        pic: "https://rukminim1.flixcart.com/image/660/792/kzzw5u80/short/v/w/v/s-tbl-shortt-tripr-original-imagbvyaftsgyudb.jpeg?q=50",
+    },
+    {
+        id: 9,
+        product_name: "Printed Men Black Regular Shorts",
+        price: 229,
+        category: "Shorts",
+        pic: "https://rukminim1.flixcart.com/image/660/792/l01blow0/short/h/s/0/xl-tblshortabs1-tripr-original-imagbwnwhkdh8and.jpeg?q=50",
+    },
+    {
+        id: 10,
+        product_name: "Striped Men Round Neck Maroon",
+        price: 159,
+        category: "T-Shirt",
+        pic: "https://rukminim1.flixcart.com/image/660/792/keykscw0-0/t-shirt/l/d/q/3xl-bmrgyrnful-z12-blive-original-imafvgzkyjghf7ba.jpeg?q=50",
+    },
+    {
+        id: 11,
+        product_name: "Solid Men Collared Neck Green T-Shirt",
+        price: 861,
+        category: "T-Shirt",
+        pic: "https://rukminim1.flixcart.com/image/660/792/kuipea80/t-shirt/e/d/x/s-ustsh0548-u-s-polo-assn-original-imag7mnrydqxwez2.jpeg?q=50",
+    },
+    {
+        id: 12,
+        product_name: "Skinny Men Blue Jeans",
+        price: 759,
+        category: "Jeans",
+        pic: "https://rukminim1.flixcart.com/image/660/792/ke0a7ww0-0/jean/b/w/8/28-11274476-roadster-original-imafusgzfqds3hmz.jpeg?q=50",
+    },
+    {
+        id: 13,
+        product_name: "Solid Men Dark Grey Sports Shorts",
+        price: 289,
+        category: "Shorts",
+        pic: "https://rukminim1.flixcart.com/image/660/792/kkwwu4w0/short/n/w/5/m-ic-4046-indiclub-original-imagy5tnm3us9wbv.jpeg?q=50",
+    },
+    {
+        id: 14,
+        product_name: "Men Slim Fit Solid Spread Collar",
+        price: 399,
+        category: "Formal Shirt",
+        pic: "https://rukminim1.flixcart.com/image/660/792/krntoy80/shirt/i/t/n/m-maroon-5-jai-textiles-original-imag5efnjxs6bmhp.jpeg?q=50",
+    },
+    {
+        id: 15,
+        product_name: "Skinny Men Blue Jeans",
+        price: 759,
+        category: "Jeans",
+        pic: "https://rukminim1.flixcart.com/image/660/792/jvtujrk0/jean/w/u/m/32-8813589-roadster-original-imafgmsbtbbvgw3s.jpeg?q=50",
+    },
+    {
+        id: 16,
+        product_name: "Men Slim Fit Solid Formal Shirt",
+        price: 501,
+        category: "Formal Shirt",
+        pic: "https://rukminim1.flixcart.com/image/660/792/kh80vww0-0/shirt/z/q/e/xxl-fo-shrt-499-drkgry-5th-anfold-original-imafxamjqyygfhmw.jpeg?q=50",
+    },
+    {
+        id:17,
+        product_name:"Solid Men Round Neck Black T-Shirt",
+        price:400,
+        category:"T-Shirt",
+        pic:"https://rukminim1.flixcart.com/image/612/612/l4n2oi80/t-shirt/b/3/r/s-1001-madfrog-original-imagfhwbsjuu3srx.jpeg?q=70"
+    },
+    {
+        id:18,
+        product_name:"Men Regular Fit Solid Spread Collar Formal Shirt",
+        price:499,
+        category:"Formal Shirt",
+        pic:"https://rukminim1.flixcart.com/image/612/612/xif0q/shirt/i/e/k/l-fbrml-rr-fibermill-original-imaghf7cf6jfxkup.jpeg?q=70"
+    },
+    {
+        id:19,
+        product_name:"Pack of 2 Solid Men Black, Grey Basic Shorts",
+        price:335,
+        category:"Shorts",
+        pic:"https://rukminim1.flixcart.com/image/612/612/l1zc6fk0/short/k/8/b/l-solid-men-black-light-grey-sports-short-adrenex-original-imagdf9hrdjeuevj.jpeg?q=70"
+    },
+    {
+        id:20,
+        product_name:"Men Slim High Rise Blue Jeans",
+        price:500,
+        category:"Jeans",
+        pic:"https://rukminim1.flixcart.com/image/612/612/kfeamq80-0/jean/b/u/c/36-suplpsjean-lblue-supernova-inc-original-imafvvajnhrvpqhw.jpeg?q=70"
+    }
+    
+]
+
+
+
+
+
+function displayData(arr){
+
+    document.getElementById("card_product").innerHTML=""
+
+    arr.forEach((product,index)=>{
+
+        let card = document.createElement("div");
+        card.classList.add("card_item");       
+
+        let imgContainer = document.createElement("div");
+        imgContainer.classList.add("img_container");
+
+        let img = document.createElement("img")
+        img.setAttribute("src",product.pic);
+        imgContainer.appendChild(img);
+
+
+        let product_info = document.createElement("div");
+        product_info.classList.add("product_info");
+
+        let title = document.createElement("h2");
+        title.append(product.product_name);
+        product_info.appendChild(title)
+
+        let para = document.createElement("p");
+        para.append("₹ "+product.price);
+        product_info.appendChild(para)
+
+
+        card.appendChild(imgContainer)
+        card.appendChild(product_info)
+
+        document.getElementById("card_product").appendChild(card);
+    })
+}
+
+displayData(products)
+
+
+
+let filterStatus = false;
+
+function openFilter(){
+
+    if(filterStatus===false){
+        document.getElementById("filter").style.marginLeft="0%"
+        filterStatus=true;
+    }
+    else{
+        document.getElementById("filter").style.marginLeft="-30%"
+        filterStatus=false;
+    }
+}
+
+
+
+
+let filters = {
+    product_name:null,
+    category:null,
+    minPrice:null,
+    maxPrice:null   
+}
+
+function setFilter(property,value){
+    if(value!==""){
+        filters[property] =value;
+    }
+    else{
+        filters[property]=null
+    }
+}
+
+
+
+
+
+function filterData(){
+    let filterArr = products;
+
+    if(filters.product_name!==null){
+        filterArr = filterArr.filter((product,index)=>{
+            return product.product_name.toLowerCase().includes(filters.product_name.toLowerCase())
+        })
+    }
+
+
+    if(filters.category!==null){
+        filterArr = filterArr.filter((product,index)=>{
+            return product.category.toLowerCase() === filters.category.toLowerCase()
+        })
+    }
+
+    if(filters.minPrice!==null){
+        filterArr = filterArr.filter((product,index)=>{
+            return Number(product.price) >= Number(filters.minPrice);
+        })
+    }
+
+    if(filters.maxPrice!==null){
+        filterArr = filterArr.filter((product,index)=>{
+            return Number(product.price)<= Number(filters.maxPrice);
+        })
+    }
+
+    displayData(filterArr);
+    console.log(filterArr)
+
+
+}
